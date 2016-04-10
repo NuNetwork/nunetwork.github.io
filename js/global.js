18d83f754d96efd91714858669dd947c4f88286f
Matt <matt@hardcoredev.com>
2016-04-10 15:50:12 -0400

    remove debug; remove large assets

--- js/global.js
+++ js/global.js
@@ -35,7 +35,6 @@
 
   $(document).ready(function(){
     if ($('body').hasClass('front')) {
-    console.log('rinnong');
     
 			// Video - Open
 			$('#hero-content .btn-video').on('click', function(){
